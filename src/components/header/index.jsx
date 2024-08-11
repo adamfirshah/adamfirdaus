f2bfc00c56c6720fb2949c394d80b26d40b6b54b
adam <adam.firshah@gmail.com>
2024-08-11 23:05:18 +0800

    intial commit

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,26 +1,27 @@
 'use client';
-import {useState} from 'react'
+import { useState } from 'react';
 import styles from './style.module.scss';
 import Burger from './burger';
 import Stairs from './stairs';
 import Menu from './menu';
 import { AnimatePresence } from 'framer-motion';
 
-export default function () {
-
+function Header() {
     const [menuIsOpen, setMenuIsOpen] = useState(false);
 
     return (
         <div className={styles.header}>
-            <Burger openMenu={() => {setMenuIsOpen(true)}}/>
+            <Burger openMenu={() => setMenuIsOpen(true)} />
             <AnimatePresence mode="wait">
-            {
-                menuIsOpen && <>
-                <Stairs />
-                <Menu closeMenu={() => {setMenuIsOpen(false)}}/>
-                </>
-            }
+                {menuIsOpen && (
+                    <>
+                        <Stairs />
+                        <Menu closeMenu={() => setMenuIsOpen(false)} />
+                    </>
+                )}
             </AnimatePresence>
         </div>
-    )
+    );
 }
+
+export default Header;
--- src/components/header/stairs/index.jsx
+++ src/components/header/stairs/index.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from 'react';
 import styles from './style.module.scss';
 import { height, background, mountAnim } from '../anim';
 import { motion } from 'framer-motion';
 
-export default function index() {
-
-  return (
-    <motion.div className={styles.stairs}>
-      {
-        [...Array(5)].map( (_, index) => {
-          return <motion.div 
-          variants={height} 
-          {...mountAnim}
-          custom={4 - index} 
-          className={styles.stair}>
-          </motion.div>
-        })
-      }
-      <motion.div 
-      variants={background} 
-      {...mountAnim}
-      className={styles.background}>
-      </motion.div>
-    </motion.div>
-  )
+export default function Stairs() {
+    return (
+        <motion.div className={styles.stairs}>
+            {[...Array(5)].map((_, index) => {
+                return (
+                    <motion.div
+                        key={index}  // Added key prop
+                        variants={height}
+                        {...mountAnim}
+                        custom={4 - index}
+                        className={styles.stair}
+                    />
+                );
+            })}
+            <motion.div
+                variants={background}
+                {...mountAnim}
+                className={styles.background}
+            />
+        </motion.div>
+    );
 }
