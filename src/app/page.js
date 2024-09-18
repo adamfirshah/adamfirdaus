3a202f748d5eba737dd6894b97da6474094d3dd0
Adam Firdaus <adamfirdaus@aqilahs-MacBook-Pro.local>
2024-09-18 13:50:06 +0800

    new base, new interfaces

--- src/app/page.js
+++ src/app/page.js
@@ -1,62 +1,43 @@
-'use client'
-import Image from 'next/image'
+'use client';
 import styles from './page.module.css'
-import { useRef, useEffect } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
+import { useEffect, useState } from 'react'
+import { AnimatePresence } from 'framer-motion';
+import Preloader from '../components/Preloader';
+import Landing from '../components/Landing';
+import Projects from '../components/Projects';
+import Description from '../components/Description';
+import SlidingImages from '../components/SlidingImages';
+import Contact from '../components/Contact';
 
 export default function Home() {
 
-  const firstText = useRef(null);
-  const secondText = useRef(null);
-  const slider = useRef(null);
-  let xPercent = 0;
-  let direction = -1;
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect( () => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to(slider.current, {
-      scrollTrigger: {
-        trigger: document.documentElement,
-        scrub: 0.25,
-        start: 0,
-        end: window.innerHeight,
-        onUpdate: e => direction = e.direction * -1
-      },
-      x: "-500px",
-    })
-    requestAnimationFrame(animate);
-  }, [])
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
 
-  const animate = () => {
-    if(xPercent < -100){
-      xPercent = 0;
-    }
-    else if(xPercent > 0){
-      xPercent = -100;
-    }
-    gsap.set(firstText.current, {xPercent: xPercent})
-    gsap.set(secondText.current, {xPercent: xPercent})
-    requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
-  }
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2000)
+      }
+    )()
+  }, [])
 
   return (
     <main className={styles.main}>
-         <video autoPlay muted loop>
-          <source src="/images/background.mp4" type="video/mp4" />
-        </video>
-       {/* <Image 
-        src="/images/background.mp4"
-        fill={true}
-        alt="background"
-      /> */}
-      <div className={styles.sliderContainer}>
-        <div ref={slider} className={styles.slider}>
-          <p ref={firstText}>Developer & Designer -</p>
-          <p ref={secondText}>Developer & Designer -</p>
-        </div>
-      </div>
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+      <Landing />
+      <Description />
+      <Projects />
+      <SlidingImages />
+      <Contact />
     </main>
   )
 }
