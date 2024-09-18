0077f4e3f69576e68b6915eaa497356aff605170
Adam Firdaus <adamfirdaus@aqilahs-MacBook-Pro.local>
2024-09-18 15:35:19 +0800

    fix some erros v9

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Adam Firdaus',
-  description: 'Portfolio Adam Firdaus',
+  description: 'Portfolio of Adam Firdaus',
 }
 
 export default function RootLayout({ children }) {
