6197cb06b9e74f081cc8bddf6a57022112dc116a
Adam Firdaus <adamfirdaus@aqilahs-MacBook-Pro.local>
2024-09-18 16:31:46 +0800

    fix v14

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google';
-// import Header from '../components/Header';
+import Header from '../components/Header/index';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Header /> */}
+        <Header />
         {children}
       </body>
     </html>
