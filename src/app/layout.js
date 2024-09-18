02b8ff26dd48ab8e2995b3187ef7ef615e0af45d
Adam Firdaus <adamfirdaus@aqilahs-MacBook-Pro.local>
2024-09-18 18:26:21 +0800

    fix v17

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google';
-import Header from '../components/Header/index.jsx';
+import Header from '../components/header/index.jsx';
 
 const inter = Inter({ subsets: ['latin'] })
 
